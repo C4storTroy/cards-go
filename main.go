530d58b00dda52d951d4c59f53efb48336305fcd
lucio <lucio.moraes@healthlink.net>
2018-08-01 16:00:33 +1200

    Initial commit to GO

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	cards := []string{"Ace of spades", newCard()}
+
+	cards = append(cards, "Six of spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+}
+
+func newCard() string {
+	return "Five of diamonds"
+}
