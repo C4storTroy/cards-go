565ad26743c63bda82e168895869ad3955086b21
lucio <lucio.moraes@healthlink.net>
2018-09-19 15:34:08 +1200

    Making a clean up on code

--- main.go
+++ main.go
@@ -4,11 +4,5 @@ func main() {
 
 	cards := newDeck() //deck{"Ace of spades", newCard()}
 
-	//cards = append(cards, "Six of spades")
-
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of diamonds"
 }
